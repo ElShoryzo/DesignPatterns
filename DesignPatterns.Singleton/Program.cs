e61f64edc811823bd8e8f04cab2b26f46c068a83
ElShoryzo <L20170831@culiacan.tecnm.mx>
2024-03-03 19:56:25 -0700

    Add (Singleton): Se agrega ejemplo Singleton

---
+++ DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatterns.Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance1 = Singleton.Instance;
+            Singleton instance2 = Singleton.Instance;
+            // Son la misma instancia a pesar de que se crearon dos objetos
+            if (instance1 == instance2)
+            {
+                System.Console.WriteLine("Objects are the same instance");
+            }
+            else
+            {
+                System.Console.WriteLine("Objects are different instances");
+            }
+            instance1.Method();
+        }
+    }
+}
